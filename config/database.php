433082ee7b476e035de08edb493abd27eacbc9b9
杨存峰 <490868351@qq.com>
2018-12-24 11:41:28 +0800

    init

--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '172.16.0.25',
+    'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'chief',
+    'database'        => 'cltdemo_db',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '!$AERGERGEFYU!@#',
+    'password'        => '',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
