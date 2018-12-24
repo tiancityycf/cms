39bd821195682ca29964fe965c3fc8912556a981
51chief <chief@51chief.com>
2018-12-24 10:37:31 +0800

    any

--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '172.16.0.25',
     // 数据库名
-    'database'        => 'cltdemo_db',
+    'database'        => 'chief',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '',
+    'password'        => '!$AERGERGEFYU!@#',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
